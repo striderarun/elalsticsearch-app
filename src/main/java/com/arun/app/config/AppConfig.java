058afe07c5da8c624c7cf551eafe396b397a224c
strider90arun <strider90arun@gmail.com>
2017-07-24 14:21:20 -0700

    Elasticsearch repository config

--- src/main/java/com/arun/app/config/AppConfig.java
+++ src/main/java/com/arun/app/config/AppConfig.java
@@ -1,11 +1,15 @@
-package com.arun.elasticsearch.config;
+package com.arun.app.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableElasticsearchRepositories(basePackages = "com.arun.app.repository")
 public class AppConfig {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AppConfig.class, args);
 	}
