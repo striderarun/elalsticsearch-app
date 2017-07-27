fe42d74093c9887b9d484fc5c661beef82516328
strider90arun <strider90arun@gmail.com>
2017-07-26 17:30:27 -0700

    Search controller endpoint

--- src/main/java/com/arun/app/rest/SearchController.java
+++ src/main/java/com/arun/app/rest/SearchController.java
@@ -1,14 +1,25 @@
 package com.arun.app.rest;
 
+import com.arun.app.beans.Book;
+import com.arun.app.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class SearchController {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    @Autowired
+    private SearchService searchService;
+
+    @RequestMapping(value = "/getDetails", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void getDetails(@RequestBody Book book) {
+
     }
 }
