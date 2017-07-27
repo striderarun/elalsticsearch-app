021b85f52ab47a25003c520347fb99cf8d773c3e
strider90arun <strider90arun@gmail.com>
2017-07-26 17:30:05 -0700

    Search Service layer

---
+++ src/main/java/com/arun/app/service/SearchService.java
@@ -0,0 +1,7 @@
+package com.arun.app.service;
+
+/**
+ * Created by arun_subramonian on 7/26/17.
+ */
+public interface SearchService {
+}
---
+++ src/main/java/com/arun/app/service/impl/SearchServiceImpl.java
@@ -0,0 +1,7 @@
+package com.arun.app.service.impl;
+
+import com.arun.app.service.SearchService;
+
+
+public class SearchServiceImpl implements SearchService {
+}
