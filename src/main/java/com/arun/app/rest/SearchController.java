c858a188198b8519d5889854e64043759cba95e9
strider90arun <strider90arun@gmail.com>
2017-07-29 22:33:31 -0700

    Find by title api

--- src/main/java/com/arun/app/rest/SearchController.java
+++ src/main/java/com/arun/app/rest/SearchController.java
@@ -3,6 +3,7 @@ package com.arun.app.rest;
 import com.arun.app.beans.Book;
 import com.arun.app.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/services/books", method = RequestMethod.POST)
@@ -29,8 +32,15 @@ public class SearchController {
 
     @RequestMapping(value = "/author/{author}", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
-    public void indexBook(@PathVariable String author, @PageableDefault Pageable pageable) {
-        searchService.findByAuthor(author, pageable);
+    public Page<Book> findBooksByAuthors(@PathVariable String author, @PageableDefault Pageable pageable) {
+        return searchService.findByAuthor(author, pageable);
     }
+
+    @RequestMapping(value = "/title/{title}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Book> findBooksByTitle(@PathVariable String title) {
+        return searchService.findByTitle(title);
+    }
+
 
 }
