46495f4af91b0cf121e638bde2d4320080779fd3
strider90arun <strider90arun@gmail.com>
2017-08-02 14:38:24 -0700

    JsonDoc constants

---
+++ src/main/java/com/arun/app/common/JsonDocConstants.java
@@ -0,0 +1,17 @@
+package com.arun.app.common;
+
+/**
+ * JSON Doc Constants
+ */
+public final class JsonDocConstants {
+
+    public static final String BOOKS = "Book";
+
+    public static final String AUTHOR_PARAM = "author";
+    public static final String AUTHOR_PARAM_DESC = "Author Name";
+
+
+    public static final String TITLE_PARAM = "title";
+    public static final String TITLE_PARAM_DESC = "Book Title";
+
+}
